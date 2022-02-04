07b48c2cba426fccccb46dd94337d1cfe376367b
Webber <sdwh@mail.john.doe.com>
2022-02-04 17:27:34 +0800

    Language Detector Finish

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 // Import all plugins
 import * as bootstrap from 'bootstrap';
+import {franc} from 'franc'
+import {iso6393} from 'iso-639-3'
+
+let get = document.getElementById('langInput');
+
+get.addEventListener('keyup', function(){
+    let langGuess = franc(document.getElementById('langInput').value, {minLength: 3});
+    let langFullName = iso6393.filter(i => i.iso6393 === langGuess)[0].name;
+
+    document.getElementById('lang-display').textContent = langFullName;
+})
